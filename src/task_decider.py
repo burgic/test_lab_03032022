09ec3b9bb1910fb969624583fb9a925a86d46745
burgic <ceburgin@gmail.com>
2022-03-03 14:49:19 +0000

    MVP End

--- src/task_decider.py
+++ src/task_decider.py
@@ -2,6 +2,23 @@
 
 def get_preferred_option(task_1 , task_2):
 
-    
+    # Scenario 1: Wash Dishes & Cook Dinner
+    if task_1.description == "Wash Dishes" and task_2.description == "Cook Dinner":
+        return task_1.description
+    elif task_1.description == "Cook Dinner" and task_2.description == "Wash Dishes":
+        return task_2.description
 
-    return task_to_do
+    # Scenario 2: Cook Dinner & Clean Windows
+    elif task_1.description == "Cook Dinner" and task_2.description == "Clean Windows":
+        return task_1.description
+    elif task_1.description == "Clean Windows" and task_2.description == "Cook Dinner":
+        return task_2.description
+
+    # Scenario 3: Clean Windows & Wash Dishes
+    elif task_1.description == "Clean Windows" and task_2.description == "Wash Dishes":
+        return task_1.description
+    elif task_1.description == "Wash Dishes" and task_2.description == "Clean Windows":
+        return task_2.description
+
+    else:
+        return task_2.description
--- tests/task_decider_test.py
+++ tests/task_decider_test.py
@@ -1,12 +1,17 @@
 import unittest
 from src.task_decider import *
+from src.task import Task
 
 class TestTask_Decider(unittest.TestCase):
     def setUp(self):
-        self.task_1 = "Wash Dishes"
-        self.task_2 = "Cook Dinner"
-        self.task_3 = "Clean Windows"
+        self.task_1 = Task("Wash Dishes", 15)
+        self.task_2 = Task("Cook Dinner", 60)
+        self.task_3 = Task("Clean Windows", 120)
     
     def test_if_task_1_is_passed_through(self):
         get_preferred_option(self.task_1, self.task_2)
-        self.assertEqual("Wash Dishes", self.task_1)
+        self.assertEqual("Wash Dishes", self.task_1.description)
+
+    def test_if_wash_over_dinner(self):
+        get_preferred_option(self.task_2, self.task_1)
+        self.assertEqual("Wash Dishes", self.task_1.description)
