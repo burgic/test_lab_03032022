8f3ba027b66b0f32bf9f769bdcdfb51dca3233d9
burgic <ceburgin@gmail.com>
2022-03-03 13:27:05 +0000

    2nd duration

--- tests/task_test.py
+++ tests/task_test.py
@@ -8,7 +8,10 @@ class TestTasks(unittest.TestCase):
         self.task = Task("Wash Clothes" , 60)
     
     def test_does_test_have_decription(self):
-        self.assertEqual("Wash Clothes",self.task.description )
+        self.assertEqual("Wash Clothes",self.task.description)
+
+    def test_does_test_have_duration(self):
+        self.assertEqual(60,self.task.duration)
         
         
         
